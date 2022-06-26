430d8c37834f9c2c647f739900b5e9a576b5126c
Jesus Rojas <jarojas6524@misena.edu.co>
2022-06-26 14:36:15 -0500

    Se agrega vista nosotros

---
+++ pages/blog.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../components/Layout'
+
+const Blog = () => {
+  return (
+    <Layout
+      pagina='Blog'
+    >
+
+    </Layout>
+  )
+}
+
+export default Blog
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,13 +1,28 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import Layout from '../components/Layout'
-
+import styles from '../styles/pages/Nosotros.module.css'
 
 const Nosotros = () => {
   return (
     <Layout
       pagina='Nosotros'
     >
+      <main className='contenedor'>
+        <h2 className='heading'>Nosostros</h2>
+        <div className={styles.contenido}>
+          <Image layout='responsive' width={600} height={450} src='/img/nosotros.jpg' />
+          <div>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illo sint voluptatum, quisquam, repudiandae velit reiciendis tempore voluptate quas voluptas rem provident adipisci blanditiis, dignissimos ipsa dolorem. Odit, eius fuga!
+            </p>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis illo sint voluptatum, quisquam, repudiandae velit reiciendis tempore voluptate quas voluptas rem provident adipisci blanditiis, dignissimos ipsa dolorem. Odit, eius fuga!
+            </p>
+          </div>
 
+        </div>
+      </main>
     </Layout>
   )
 }
